d8585eec9e569270f033abb5d36e0ca2deaf5ace
Knoxportion <gustav.enstrom@gmail.com>
2016-08-26 17:13:30 +0200

    displayHero() working
    The funtion to display the hero portrait is now working.

--- javascript/script.js
+++ javascript/script.js
@@ -41,6 +41,32 @@ window.onclick = function(event) {
 }
 
 //Something like this should work. Add if statements for various classes
-function displayHero(){
-  document.getElementById("imageid").src="../template/save.png";
+function displayHero(clicked_id){
+if(clicked_id == "druidButton"){
+  document.getElementById("heroImage").src="img/druid.png";
+}
+if(clicked_id == "hunterButton"){
+  document.getElementById("heroImage").src="img/hunter.png";
+}
+if(clicked_id == "mageButton"){
+  document.getElementById("heroImage").src="img/mage.png";
+}
+if(clicked_id == "paladinButton"){
+  document.getElementById("heroImage").src="img/paladin.png";
+}
+if(clicked_id == "priestButton"){
+  document.getElementById("heroImage").src="img/priest.png";
+}
+if(clicked_id == "rogueButton"){
+  document.getElementById("heroImage").src="img/rogue.png";
+}
+if(clicked_id == "shamanButton"){
+  document.getElementById("heroImage").src="img/shaman.png";
+}
+if(clicked_id == "warlockButton"){
+  document.getElementById("heroImage").src="img/warlock.png";
+}
+if(clicked_id == "warriorButton"){
+  document.getElementById("heroImage").src="img/warrior.png";
+}
 }
